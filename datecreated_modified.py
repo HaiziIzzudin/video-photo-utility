bb0a0793fcfc106034f02680e54ca6d9990fe520
haizi <thehub@iziizz.slmail.me>
2024-07-25 02:17:19 +0800

    updattee

--- datecreated_modified.py
+++ datecreated_modified.py
@@ -1,44 +1,40 @@
 from filesIngest import filesIngest
 from change_modtime import changeTo
 
-import re
 from prettytable import PrettyTable
 import os
 from colorama import just_fix_windows_console
 just_fix_windows_console()
 from colorama import Fore, Style
 from time import sleep
-import platform
-import time
 from datetime import datetime
-
-if platform.system() == 'Windows':
-  import win32file
-  import pywintypes
-  import win32con
-
 
 
 table = PrettyTable()
-table.field_names = ["Filename", "Datetime Taken/Encoded", "Old Date Created/Mod", "New Date Created/Mod"]
+table.field_names = ["No.", "Filename", "Datetime Taken/Encoded", "Old Date Created/Mod", "New Date Created/Mod"]
+
+mediatype = 'images' # IMAGES / VIDEOS
+
 rr = filesIngest()
-rr.select_files('videos')
+rr.select_files(mediatype)
 
 
 
 
 
 
-
+index:int = 0
 for name in rr.getFileList():
-
+  index += 1
   dt_modtime = datetime.fromtimestamp(os.path.getmtime(name))
 
-  encodedtime_obj = rr.getFile_EncodedDate(name)
+  encodedtime_obj = rr.getFile_EncodedDate(name, mediatype)
 
-  table.add_row([name, encodedtime_obj.strftime("%Y-%m-%d %H:%M:%S"), dt_modtime.strftime("%Y-%m-%d %H:%M:%S"), encodedtime_obj.strftime("%Y-%m-%d %H:%M:%S")])
+  table.add_row([index, name, encodedtime_obj.strftime("%Y-%m-%d %H:%M:%S"), dt_modtime.strftime("%Y-%m-%d %H:%M:%S"), encodedtime_obj.strftime("%Y-%m-%d %H:%M:%S")])
 
 print(table)
+totaldata = index
+print(f"Total data: {totaldata}")
 
 
 
@@ -47,10 +43,12 @@ print(table)
 confirm = input("confirm change? (y / enter other to quit) ")
 
 if confirm == 'y':
+  index = 0
   for name in rr.getFileList():
-    
-    encodedtime_obj = rr.getFile_EncodedDate(name)
+    index += 1
+    encodedtime_obj = rr.getFile_EncodedDate(name, mediatype)
     changeTo(encodedtime_obj, name)
+    print(f"[{index}/{totaldata}]")
     print(Fore.GREEN + name + ' has been changed to ' + encodedtime_obj.strftime("%Y-%m-%d %H:%M:%S") + ' successfully.' + Style.RESET_ALL)
 
 else:
--- exif_or_encodedate_from_filename.py
+++ exif_or_encodedate_from_filename.py
@@ -1,4 +1,6 @@
 from filesIngest import filesIngest
+from countdown import countdown
+from raw import convert2raw
 
 import subprocess
 import os
@@ -6,6 +8,7 @@ from datetime import datetime
 from prettytable import PrettyTable
 from time import sleep
 import platform
+from send2trash import send2trash
 
 from colorama import just_fix_windows_console
 just_fix_windows_console()
@@ -20,27 +23,29 @@ if platform.system() == 'Windows':
 
 
 exiftool_location = r"C:\Program Files\XnViewMP\AddOn"
+mediatype = 'images' ### IMAGES / VIDEOS
 
 
 # change directory to the exiftool location first
 os.chdir(exiftool_location)
 
 
+
 def getImageDate(filepath):
   date_str = os.path.splitext(os.path.basename(filepath))[0].split("_")[1]
   time_str = os.path.splitext(os.path.basename(filepath))[0].split("_")[2]
-  date_obj = datetime.strptime(date_str, "%Y%m%d")
+  date_obj = datetime.strptime(date_str, "%Y%m%d")  ## this must be true otherwise ERR
   
   try:
     time_obj = datetime.strptime(time_str, "%H%M%S")
-    return (date_obj, time_obj)
   
   except:
-    print(Fore.YELLOW + 'Error parsing time string '+ time_str+ ', possibly invalid character. Replacing with 00:00:00')
+    print(Fore.MAGENTA + 'Error parsing time string '+ time_str+ ', possibly invalid character. Replacing with 00:00:00' + Style.RESET_ALL)
     time_str = '000000'
     time_obj = datetime.strptime(time_str, "%H%M%S")
-    return (date_obj, time_obj)
   
+  datetime_obj = datetime.combine(date_obj.date(), time_obj.time())
+  return datetime_obj
 
 
 
@@ -56,21 +61,24 @@ img = filesIngest()
 
 # init prettytable
 table = PrettyTable()
-table.field_names = ["File Path", "File Name", "File Date Extract"]
+table.field_names = ["No.", "File Path", "File Name", "File Datetime Extract"]
 
 
 # prompt to get files list
-img.select_files('images')
+img.select_files(mediatype)
 
 
-
+count: int = 0
 # for listing in table
 for i in img.getFileList():
-  date_obj = getImageDate(i)
-  date_formatted = date_obj.strftime("%Y-%m-%d")
-  table.add_row([i, os.path.basename(i), date_formatted])
+  count += 1
+  datetime_obj = getImageDate(i)
+  date_formatted = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+  table.add_row([count, i, os.path.basename(i), date_formatted])
   
 print(table)
+finalcount = count
+print(f"Total file: {finalcount}")
 
 
 # prompt to user what they are looking here.
@@ -100,7 +108,6 @@ while True:
     print(Style.RESET_ALL)
     exit(0)
   elif confirm2 == 'y':
-    print(Fore.YELLOW + 'Proceed with next instructions.')
     break
   else:
     print(Fore.RED + 'Invalid input. Please try again.')
@@ -109,25 +116,46 @@ while True:
 print(Style.RESET_ALL)
 
 
-
+count:int = 0
 for j in img.getFileList():
-  
+  count += 1
   try:
     
+    datetime_obj = getImageDate(j)
 
-    ## change EXIF trio dates
 
-    dateobj = getImageDate(j)
-    command = f'./exiftool -AllDates="{dateobj}" -overwrite_original "{j}"'
-    ### YES WINDOWS TERMINAL CAN ONLY INTERPRET DOUBLE QUOTE AS ENCLOSING FOR ANY THAT HAS SPACES!
-    print(command)
-    subprocess.run(command)
+    if mediatype == 'videos':
+      ## TODO: change date encoded
+      date_formatted = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+      tempfilename = f'{os.path.dirname(j)}/temp.mp4'
+      command = f'ffmpeg -y -i "{j}" -c copy -metadata creation_time="{date_formatted}" "{tempfilename}"'
+      ### YES WINDOWS TERMINAL CAN ONLY INTERPRET DOUBLE QUOTE AS ENCLOSING FOR ANY THAT HAS SPACES!
+      print(command)
+      print(tempfilename)
+      
+      if os.path.exists(j): countdown(Fore.YELLOW + 'File ' + j + ' will be overitten. Please cancel in', 1)
+      print(Style.RESET_ALL)
+
+      subprocess.run(command)
+      sleep(0.5)
+      send2trash( convert2raw(j) )
+      sleep(0.5)
+      os.rename( tempfilename , j )
+    
+    elif mediatype == 'images':
+      ## change EXIF trio dates
+      dateobj = getImageDate(j)
+      command = f'./exiftool -AllDates="{dateobj}" -overwrite_original "{j}"'
+      ### YES WINDOWS TERMINAL CAN ONLY INTERPRET DOUBLE QUOTE AS ENCLOSING FOR ANY THAT HAS SPACES!
+      print(f"[{count}/{finalcount}]")
+      print(command)
+      subprocess.run(command)
     
 
 
     ## change THE file creation date and modified date
     
-    timestamp = dateobj.timestamp()
+    timestamp = datetime_obj.timestamp()
     if platform.system() == 'Windows':
       # Convert timestamp to Windows file time
       wintime = pywintypes.Time(timestamp)
@@ -142,7 +170,7 @@ for j in img.getFileList():
     
     
     ## print success statement
-    print(Fore.GREEN + os.path.basename(j) + ' EXIF data has been changed to ' + dateobj.strftime("%Y-%m-%d"))
+    print(Fore.GREEN + os.path.basename(j) + ' video data has been changed to ' + datetime_obj.strftime("%d/%m/%Y"))
     print(Style.RESET_ALL)
     sleep(0.2)
   
